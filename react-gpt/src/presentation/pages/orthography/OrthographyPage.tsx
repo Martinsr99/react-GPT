df464f5d96c5950b9300cfd1f027d0ea4c911a2c
Martinsr99 <silesreche.martin@gmail.com>
2024-11-07 19:49:36 +0100

    feat: add chat bubbles

--- react-gpt/src/presentation/pages/orthography/OrthographyPage.tsx
+++ react-gpt/src/presentation/pages/orthography/OrthographyPage.tsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import { GptMessage, MyMessage } from '../../components'
 
 export const OrthographyPage = () => {
   return (
-    <div>OrthographyPage</div>
+    <div className='chat-container'>
+      <div className='chat-messages'>
+        <div className='grid grid-cols-12 gap-y-2'>
+          <GptMessage text="Introduce tu texto para empezar a ayudarte" />
+          <MyMessage text="Hola Mundo"/>
+
+        </div>
+      </div>
+    </div>
   )
 }
