1bc2ffbcb7f12b7506a088d4f4e5ca36be716129
Martinsr99 <silesreche.martin@gmail.com>
2025-02-20 21:09:58 +0100

    add pros cons first version

--- react-gpt/src/presentation/pages/pros-cons/ProsConsPage.tsx
+++ react-gpt/src/presentation/pages/pros-cons/ProsConsPage.tsx
@@ -1,7 +1,46 @@
-import React from 'react'
+import { useState } from 'react';
+import { GptMessage, MyMessage, TextMessageBox, TypingLoader } from '../../components';
+
+interface Message {
+  text: string;
+  isGpt: boolean
+}
 
 export const ProsConsPage = () => {
+
+  const [isLoading, setIsLoading] = useState(false)
+  const [messages, setMessages] = useState<Message[]>([])
+
+  const handlePost = async (text: string) => {
+    setIsLoading(true)
+    setMessages((prev) => [...prev, {text,isGpt:false}])
+
+    //TODO: useCase
+    setIsLoading(false)
+
+    //TODO: add isGpt to true
+  }
+
   return (
-    <div>ProsConsPage</div>
+    <div className='chat-container'>
+      <div className='chat-messages'>
+        <div className='grid grid-cols-12 gap-y-2'>
+          <GptMessage text="Introduce tu texto para empezar a ayudarte" />
+          {
+            messages.map((message ,index) => (
+              message.isGpt ? (<GptMessage key={index} text='Esto es de OpenAI' />) : <MyMessage key={index} text={message.text} />
+            ))
+          }
+          {
+            isLoading && (          <div className="col-start-1 col-end-12 fade-in" >
+              <TypingLoader/>
+              </div>)
+          }
+
+
+        </div>
+      </div>
+      <TextMessageBox onSendMessage={handlePost} placeholder='Escribe aqui lo que deseas' disableCorrections/>
+    </div>
   )
 }
