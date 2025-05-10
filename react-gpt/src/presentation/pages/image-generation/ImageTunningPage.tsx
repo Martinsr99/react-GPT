095b991a9566fc79193058aa4b0155dec4264018
Martinsr99 <silesreche.martin@gmail.com>
2025-05-10 21:59:24 +0200

    feat: add GptMessageSelectableImage component for image selection and editing

--- react-gpt/src/presentation/pages/image-generation/ImageTunningPage.tsx
+++ react-gpt/src/presentation/pages/image-generation/ImageTunningPage.tsx
@@ -1,81 +1,123 @@
-import { useState } from 'react'
-import { GptMessage, MyMessage, TypingLoader, TextMessageBox, GptMessageImage } from '../../components';
-import { imageGenerationUseCase } from '../../../core/use-cases';
+import { useState } from "react";
+import {
+  GptMessage,
+  MyMessage,
+  TypingLoader,
+  TextMessageBox,
+  GptMessageImage,
+} from "../../components";
+import { imageGenerationUseCase } from "../../../core/use-cases";
+import { GptMessageSelectedImage } from "../../components/chat-bubbles/GptMessageSelectableImage";
 
 interface Message {
   text: string;
-  isGpt: boolean
+  isGpt: boolean;
   info?: {
-    imageUrl: string
-    alt: string
-  }
+    imageUrl: string;
+    alt: string;
+  };
 }
 
 export const ImageTunningPage = () => {
-
-  const [isLoading, setIsLoading] = useState(false)
-  const [messages, setMessages] = useState<Message[]>([])
+  const [isLoading, setIsLoading] = useState(false);
+  const [messages, setMessages] = useState<Message[]>([]);
 
   const [originalImageAndMask, setOriginalImageAndMask] = useState({
     original: undefined as string | undefined,
-    mask: undefined as string | undefined
-  })
+    mask: undefined as string | undefined,
+  });
 
-  const handleVariation = async() => {
-    setIsLoading(true)
-    const resp = await imageGenerationUseCase(originalImageAndMask.original!)
-    setIsLoading(false)
-    if(!resp) return
+  const handleVariation = async () => {
+    setIsLoading(true);
+    const resp = await imageGenerationUseCase(originalImageAndMask.original!);
+    setIsLoading(false);
+    if (!resp) return;
 
-    setMessages((prev) => [...prev, {text:'Variación',isGpt:true, info:{imageUrl:resp.url, alt:resp.alt}}])
-  }
+    setMessages((prev) => [
+      ...prev,
+      {
+        text: "Variación",
+        isGpt: true,
+        info: { imageUrl: resp.url, alt: resp.alt },
+      },
+    ]);
+  };
 
   const handlePost = async (text: string) => {
-    setIsLoading(true)
-    setMessages((prev) => [...prev, {text,isGpt:false}])
+    setIsLoading(true);
+    setMessages((prev) => [...prev, { text, isGpt: false }]);
 
-    const imageInfo = await imageGenerationUseCase(text)
-    
-    setIsLoading(false)
+    const imageInfo = await imageGenerationUseCase(text);
+
+    setIsLoading(false);
 
     //TODO: add isGpt to true
-    if(!imageInfo) return setMessages((prev) => [...prev, {text:'No se pudo generar la imagen',isGpt:true}])
+    if (!imageInfo)
+      return setMessages((prev) => [
+        ...prev,
+        { text: "No se pudo generar la imagen", isGpt: true },
+      ]);
 
-    setMessages((prev) => [...prev, {text,isGpt:true,info:{imageUrl:imageInfo.url, alt:imageInfo.alt}}])
-
-
-  }
+    setMessages((prev) => [
+      ...prev,
+      {
+        text,
+        isGpt: true,
+        info: { imageUrl: imageInfo.url, alt: imageInfo.alt },
+      },
+    ]);
+  };
 
   return (
     <>
-    {originalImageAndMask.original && (
-      <div className='fixed flex-col items-center top-10 right-10 z-10 fade-in'>
-        <span>Editando</span>
-        <img src={originalImageAndMask.original} alt='Imagen original' className='border rounded-xl w-36 h-36 object-contain'/>
-        <button onClick={handleVariation} className='btn-primary mt-2'>Generar variación</button>
-      </div>
-    )}
-        <div className='chat-container'>
-      <div className='chat-messages'>
-        <div className='grid grid-cols-12 gap-y-2'>
-          <GptMessage text="¿Qué imagen quieres generar?" />
-          {
-            messages.map((message ,index) => (
-              message.isGpt ? (<GptMessageImage key={index} imageUrl={message.info?.imageUrl!} alt={message.info?.alt!} />) : <MyMessage key={index} text={message.text} />
-            ))
-          }
-          {
-            isLoading && (          <div className="col-start-1 col-end-12 fade-in" >
-              <TypingLoader/>
-              </div>)
-          }
-
-
+      {originalImageAndMask.original && (
+        <div className="fixed flex-col items-center top-10 right-10 z-10 fade-in">
+          <span>Editando</span>
+          <img
+            src={originalImageAndMask.original}
+            alt="Imagen original"
+            className="border rounded-xl w-36 h-36 object-contain"
+          />
+          <button onClick={handleVariation} className="btn-primary mt-2">
+            Generar variación
+          </button>
         </div>
-      </div>
-      <TextMessageBox onSendMessage={handlePost} placeholder='Escribe aqui lo que deseas' disableCorrections/>
-    </div>
-    </>
+      )}
+      <div className="chat-container">
+        <div className="chat-messages">
+          <div className="grid grid-cols-12 gap-y-2">
+            <GptMessage text="¿Qué imagen quieres generar?" />
+            {messages.map((message, index) =>
+              message.isGpt ? (
+                <GptMessageSelectedImage
+                  key={index}
+                  imageUrl={message.info?.imageUrl!}
+                  alt={message.info?.alt!}
+                  onImageSelected={(url) =>
+                    setOriginalImageAndMask({
+                      original: message.info?.imageUrl,
+                      mask: url,
+                    })
+                  }
+                />
+              ) : (
+                <MyMessage key={index} text={message.text} />
+              )
+            )}
 
-  )
-}
+            {isLoading && (
+              <div className="col-start-1 col-end-12 fade-in">
+                <TypingLoader />
+              </div>
+            )}
+          </div>
+        </div>
+        <TextMessageBox
+          onSendMessage={handlePost}
+          placeholder="Escribe aqui lo que deseas"
+          disableCorrections
+        />
+      </div>
+    </>
+  );
+};
